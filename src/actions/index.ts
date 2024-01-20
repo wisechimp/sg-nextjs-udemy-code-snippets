e1b9e71ce988ea16c1e8e52d629a770a365ec36e
shicks1080 <stuart.hicks@gmail.com>
2024-01-20 17:40:34 +0100

    housekeeping: Renaming the server actions folder
    As actions rather than handlers...

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,6 @@
 import getSnippet from "./getSnippet"
 import getSnippets from "./getSnippets"
 import postSnippet from "./postSnippet"
+import updateSnippet from "./updateSnippet"
 
-export { getSnippet, getSnippets, postSnippet }
+export { getSnippet, getSnippets, postSnippet, updateSnippet }
